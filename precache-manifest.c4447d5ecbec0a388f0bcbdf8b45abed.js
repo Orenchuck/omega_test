bd269450b6c81996fcf48756869d7503c281e53f
orenchuck <golub.ptica@gmail.com>
2020-08-19 17:42:03 +0300

    Updates

---
+++ precache-manifest.c4447d5ecbec0a388f0bcbdf8b45abed.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cba33a21a1573702e48cfc7ec825ea2",
+    "url": "/omega_test/index.html"
+  },
+  {
+    "revision": "6c6e532832a95276a19e",
+    "url": "/omega_test/static/css/main.e75ff0ab.chunk.css"
+  },
+  {
+    "revision": "b880f3ef5f65e97f32c1",
+    "url": "/omega_test/static/js/2.561ab259.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/omega_test/static/js/2.561ab259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6e532832a95276a19e",
+    "url": "/omega_test/static/js/main.9a1749bd.chunk.js"
+  },
+  {
+    "revision": "b011cb2105302beb1cc5",
+    "url": "/omega_test/static/js/runtime-main.84dcc8e0.js"
+  },
+  {
+    "revision": "41104523745008a7f7714c1bb83f7868",
+    "url": "/omega_test/static/media/1.41104523.png"
+  },
+  {
+    "revision": "60ca41fc67be47ce5e03281f7b59b658",
+    "url": "/omega_test/static/media/2.60ca41fc.png"
+  },
+  {
+    "revision": "35cd3df86b0ee92caedd4e1ee86677fc",
+    "url": "/omega_test/static/media/3.35cd3df8.png"
+  },
+  {
+    "revision": "2de3bb3d3ac6c452d2d47ed3a422b780",
+    "url": "/omega_test/static/media/4.2de3bb3d.png"
+  }
+]);
